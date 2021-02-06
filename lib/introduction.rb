90dbac4b66dead7a96e22f1d98afaa07f4a64663
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-06 06:04:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,22 +7,13 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-# introduction (Luis)
-
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-# introduction_with_language (Luis, Ruby)
-
 def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-
-# introduction_with_language_optional (Luis, Python)
-
-# introduction_with_language_optional (Luis)
-
 
 
 
